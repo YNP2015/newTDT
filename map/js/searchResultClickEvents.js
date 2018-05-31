01b7e8de9f53c0bb90978047e5c5112a108649e4
YNP <ynpchn@163.com>
2018-05-31 15:37:29 +0800

    enter

--- map/js/searchResultClickEvents.js
+++ map/js/searchResultClickEvents.js
@@ -1,7 +1,7 @@
 /* poi查询之后地图上坐标点的点击事件 */
 
 function clickSearchResultPanel(smx, smy, num) {
-    map.setCenter(new SuperMap.LonLat(smx, smy), 7);
+    map.setCenter(new SuperMap.LonLat(smx, smy), 15);
     poiPointSelect(tenFeatursList[num]);
 }
 
